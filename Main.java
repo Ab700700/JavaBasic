05f02c08ecf732ba25391c64df3e0f032d7b4987
Ab700700 <148686321+Ab700700@users.noreply.github.com>
2023-10-24 04:28:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+                // 1. Develop a program that takes the weight (in kilograms) and height (in meters) as
+                //input and calculates the BMI, then prints it
+
+
+//        System.out.println("Please enter your weight: ");
+//        double weight = s.nextDouble();
+//        System.out.println("Please enter your height: ");
+//        double height = s.nextDouble();
+//        double  bmi = weight/(height*height);
+//        System.out.println("BMI = "+bmi);
+
+
+        //2. Write a program that takes the obtained marks and total marks as input and
+        //calculates the percentage, then prints it
+
+//        System.out.println("Enter obtained marks = ");
+//        double obtainedMarks = s.nextDouble();
+//        System.out.println("Enter Total marks = ");
+//        double TotalMarks = s.nextDouble();
+//             double percentage = (obtainedMarks/ TotalMarks)*100;
+//        System.out.println("Percentage = "+percentage+"%");
+
+        //3. Create a program that takes an amount in one currency and an exchange rate as
+        //input, then converts and prints the amount in another currency.
+
+//        System.out.println("From: ");
+//        String currencyOne = s.next();
+//        System.out.println("To: ");
+//        String currencyTwo = s.next();
+//        System.out.println("Enter amount in "+currencyOne+" = ");
+//        double amount = s.nextDouble();
+//        System.out.println("Enter exchangeRate ("+currencyOne+" to "+currencyTwo+") = ");
+//        double exchangeRate = s.nextDouble();
+//        System.out.println("Amount in "+currencyTwo+" = "+amount* exchangeRate);
+
+
+        // 4. Create a program that takes a string as input, calculates its length, and then reverses
+        //the string using the StringBuilder class, finally printing both the length and reversed
+        //string.
+
+//        System.out.println("Enter a string:");
+//        String strInput = s.nextLine();
+//        StringBuilder str = new StringBuilder(strInput);
+//            str.reverse();
+//        System.out.println("Length of the string: "+strInput.length()+" And Reversed string: "+str);
+
+        //5. Develop a program that takes a sentence as input and extracts a substring from it,
+        //then prints the extracted substring.
+
+//        System.out.println("Enter a sentance: ");
+//        String st = s.nextLine();
+//        System.out.println("start: ");
+//        int start = s.nextInt();
+//        System.out.println("end: ");
+//        int end = s.nextInt();
+//        System.out.println(st.substring(start,end));
+
+
+    //6. Write a program that takes a sentence and a keyword as input, then check if the
+    //keyword is present in the sentence and prints the result.
+
+//        System.out.println("Enter a sentance: ");
+//        String st = s.nextLine();
+//        System.out.println("Enter the keyword: ");
+//        String keyWord = s.nextLine();
+//        if(st.contains(keyWord)) System.out.println("Keyword \""+keyWord+"\" is present in the sentence");
+//        else System.out.println("Keyword \""+keyWord+"\" is not present in the sentence");
+
+
+        //7. Develop a program that takes a sentence and a word to replace as input, then
+        //replace all occurrences of the word with another word and prints the modified
+        //sentence.
+
+//        System.out.println("Enter a sentence: ");
+//        String st = s.nextLine();
+//        System.out.println("Enter a replace word: ");
+//        String replace = s.next();
+//        System.out.println("Enter a replacement word: ");
+//        String replacementWord = s.next();
+//        System.out.println(st.replaceAll(replace,replacementWord));
+
+//        8. Write a program that takes two strings as input and check if they are equal, ignoring
+//        the case, then prints whether they are equal or not.
+
+//        System.out.println("Enter string1: ");
+//        String str1 = s.next();
+//        System.out.println("Enter string2: ");
+//        String str2 = s.next();
+//        if(str1.equalsIgnoreCase(str2)) System.out.println("String are equal (ignoring case).");
+//        else System.out.println("String are not equal (ignoring case).");
+    }
+
+    }
